a5abe8bd17651db4b0f63464a1fc9258988b92fe
Vinicius_Vatanabi <viniciusvatanabi@gmail.com>
2022-01-25 16:17:53 -0300

    Initial commit

---
+++ Paradigma.Internet/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Paradigma.Internet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
