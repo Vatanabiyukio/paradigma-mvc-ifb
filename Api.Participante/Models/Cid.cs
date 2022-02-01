e1d751630bfad8363a7da7f64d9e748d943c011e
Vinicius_Vatanabi <viniciusvatanabi@gmail.com>
2022-02-01 15:02:01 -0300

    Adicionada classe Cid.cs e classe Sangue.cs

---
+++ Api.Participante/Models/Cid.cs
@@ -0,0 +1,7 @@
+namespace Api.Participante.Models;
+
+public class Cid
+{
+    public string Cid { get; set; }
+    public string Descrição { get; set; }
+}
---
+++ Api.Participante/Models/Sangue.cs
@@ -0,0 +1,7 @@
+namespace Api.Participante.Models;
+
+public class Sangue
+{
+    public char Tipo { get; set; }
+    public char FatorRh { get; set; }
+}
