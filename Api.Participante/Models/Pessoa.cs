8d59abf32f87240b33e652d9168048e3ae0e6693
Vinicius_Vatanabi <viniciusvatanabi@gmail.com>
2022-02-01 14:50:27 -0300

    Adicionado novos campos a classe Pessoa.cs

--- Api.Participante/Models/Pessoa.cs
+++ Api.Participante/Models/Pessoa.cs
@@ -4,14 +4,52 @@ namespace Api.Participante.Models;
 
 public class Pessoa
 {
+    // ========= Dados Gerais =========
     [Key]
     public long Handle { get; set; }
     [Required]
     public string Nome { get; set; }
     [Required]
     public int Cpf { get; set; }
+    
+    // ========= Contato =========
     [Required]
     public int Telefone { get; set; }
     [Required]
     public int Email { get; set; }
+    public PreferenciaContato? Preferencia { get; set; }
+    
+    // ========= Contato de Emergência =========
+    public int? TelefoneEmergencia { get; set; }
+
+    // ========= Endereço =========
+    public int? Cep { get; set; }
+    public string? Logradouro { get; set; }
+    public string? Complemento { get; set; }
+    public string? Referencia { get; set; }
+    public Uf? Uf { get; set; }
+    public string? Cidade { get; set; }
+
+    // ========= Dados Convenio =========
+    [Required]
+    public bool? Conveniado { get; set; }
+    [Conveniado]
+    public string? NomeConvenio { get; set; }
+    [Conveniado]
+    public int? MatriculaConvenio { get; set; }
+    [Conveniado]
+    public DateOnly? ValidadeConvenio { get; set; }
+}
+
+public class ConveniadoAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var pessoa = (Pessoa) validationContext.ObjectInstance;
+        if (pessoa.Conveniado.Equals(true))
+            return ValidationResult.Success;
+
+        var campo = value as string;
+        return (string.IsNullOrWhiteSpace(campo) ? new ValidationResult("Value is required.") : ValidationResult.Success);
+    }
 }
