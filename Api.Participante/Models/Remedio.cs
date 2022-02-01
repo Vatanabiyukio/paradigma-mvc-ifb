340ee134c522f8142d8a1d53e04903f272ac95cf
Vinicius_Vatanabi <viniciusvatanabi@gmail.com>
2022-02-01 15:21:15 -0300

    Adicionada classe Remedio.cs

---
+++ Api.Participante/Models/Remedio.cs
@@ -0,0 +1,8 @@
+namespace Api.Participante.Models;
+
+public class Remedio
+{
+    public string Codigo { get; set; }
+    public string? Descricao { get; set; }
+    public bool? Controlado { get; set; }
+}
